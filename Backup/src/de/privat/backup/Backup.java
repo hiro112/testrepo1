f836c5de32427aa41c4f5fc8341ae02dba13cf70
Nicolas <Nicolas@netbook.Speedport_W_723V_1_37_000>
2015-08-15 20:11:17 +0200

    Auslesen Zielpfad

--- Backup/src/de/privat/backup/Backup.java
+++ Backup/src/de/privat/backup/Backup.java
@@ -1,9 +1,27 @@
 package de.privat.backup;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Backup {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+	
+		File propertiesFile = new File("./Files/backup.properties");
+		Properties properties = new Properties();
+		 
+		if(propertiesFile.exists())
+		{
+		  BufferedInputStream bis = new BufferedInputStream(new FileInputStream(propertiesFile));
+		  properties.load(bis);
+		  bis.close();  
+		}
+		 
+		System.out.println(properties.getProperty("destinationPath"));
+
  
 	}
 
